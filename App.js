0dabc8d095f9005d45efdf8eed726dc407a60632
ER-Mollo <mollorefilwe68@gmail.com>
2022-09-19 12:38:06 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
+import { Camera } from 'expo-camera';
+import { useEffect, useRef,useState } from 'react';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
+
+  let cameraRef = useRef();
+  const [hasCameraPermission, setHasCameraPermission] = useState();
+  const [hasMediaLibrary,setHasMediaLibrary] = useState();
+  const [photo,setPhoto] = useState();
+
+  useEffect(()=>{
+    (async()=>{
+      const CameraPermission = await Camera.requestCameraPermissionsAsync();
+      const MediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+
+      setHasCameraPermission(CameraPermission.status=== "granted");
+      setHasMediaLibrary(MediaLibraryPermission.status=== "granted");
+    })();
+  },[]);
+
+  if (hasCameraPermission === undefined){
+    return<Text>Requesting permission...</Text>
+  }else if(!hasCameraPermission){
+    return(
+      <Text>permissionf for camera not granted</Text>
+    )
+  }
+
+  let takePic = async() => {
+    let options ={
+      quality:1,
+      base64:true,
+      exif: false
+    };
+
+    let newPhoto = await cameraRef.current.takePictureAsync(options);
+    setPhoto(newPhoto);
+  }
+
+  if (photo) {
+    let savePhoto =() => {
+      MediaLibrary.saveToLibraryAsync(photo.uri).then(() =>{
+        setPhoto(undefined);
+      });
+    };
+    return(
+      <SafeAreaView style={styles.container}>
+        <Image style={styles.preview} source = {{uri:"data:image/jpg;base64" + photo.base64}}/>
+        {hasMediaLibrary ? <Button title='save' onPress={savePhoto}/>: undefined}
+        <Button title='Discard' onPress={()=> setPhoto(undefined)}/>
+      </SafeAreaView>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Camera style={styles.container} ref={cameraRef}>
+      <View style={styles.buttonContainer}>
+        <Button title='take pic' onPress={takePic}/>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </Camera>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer:{
+    backgroundColor:'#fff',
+    alignSelf: 'flex-end',
+  },
+  preview:{
+    alignSelf: 'stretch',
+    flex: 1
+  }
+
 });
