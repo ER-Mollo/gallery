13c56f8ed6224f566d246dfbb2c5f648cf161b99
ER-Mollo <mollorefilwe68@gmail.com>
2022-09-27 13:17:01 +0200

    added

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Button, StyleSheet, Text, View, SafeAreaView, Image} from 'react-native
 import { Camera } from 'expo-camera';
 import { useEffect, useRef,useState } from 'react';
 import * as MediaLibrary from 'expo-media-library';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
 
@@ -10,6 +11,24 @@ export default function App() {
   const [hasCameraPermission, setHasCameraPermission] = useState();
   const [hasMediaLibrary,setHasMediaLibrary] = useState();
   const [photo,setPhoto] = useState();
+  const [image, setImage] = useState(null);
+
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
 
   useEffect(()=>{
     (async()=>{
@@ -42,25 +61,33 @@ export default function App() {
 
   if (photo) {
     let savePhoto =() => {
+      console.log(photo);
       MediaLibrary.saveToLibraryAsync(photo.uri).then(() =>{
         setPhoto(undefined);
       });
+      
     };
     return(
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.previewcontainer}>
         <Image style={styles.preview} source = {{uri:"data:image/jpg;base64" + photo.base64}}/>
-        {hasMediaLibrary ? <Button title='save' onPress={savePhoto}/>: undefined}
+        {/* {hasMediaLibrary ? <Button title='save' onPress={savePhoto}/>: undefined} */}
+        <Button title='save' onPress={savePhoto}/>
         <Button title='Discard' onPress={()=> setPhoto(undefined)}/>
+        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
       </SafeAreaView>
     )
   }
 
+ 
+
   return (
     <Camera style={styles.container} ref={cameraRef}>
-      <View style={styles.buttonContainer}>
-        <Button title='take pic' onPress={takePic}/>
-      </View>
-      <StatusBar style="auto" />
+        <View style={styles.buttonContainer}>
+          <Button title='take pic' onPress={takePic}/>
+          <Button title='view pic' onPress={takePic}/>
+        </View>
+        <StatusBar style="auto" />
     </Camera>
   );
 }
@@ -76,8 +103,10 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-end',
   },
   preview:{
-    alignSelf: 'stretch',
-    flex: 1
+    // alignSelf: 'stretch',
+    flex: 1,
+    width:'100vh',
+    height:'50vh'
   }
 
 });
